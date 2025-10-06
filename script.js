2924b93dbf7ec9a72d9b884df21e3fb35fb67c7a
Hooriya Shaikh <huriyasheikh2550@gmail.com>
2025-10-07 01:19:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
+const navToggle = document.getElementById('nav-toggle');
+const navMenu = document.getElementById('nav-menu');
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show-menu');
+});
+
+// Close menu on nav link click (mobile)
+document.querySelectorAll('.nav__link').forEach(link => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+});
+
+// ScrollReveal animations
+const sr = ScrollReveal({
+  distance: '50px',
+  duration: 800,
+  easing: 'ease',
+  reset: false,
+});
+
+sr.reveal('.home__content', { origin: 'left', interval: 200 });
+sr.reveal('.home__img-wrapper', { origin: 'right', interval: 200 });
+
+sr.reveal('.about__img', { origin: 'left' });
+sr.reveal('.about__data', { origin: 'right', delay: 200 });
+
+sr.reveal('.services__content', { origin: 'bottom', interval: 200 });
+
+sr.reveal('.contact__form', { origin: 'bottom' });
